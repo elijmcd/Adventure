50bb7582e5cdd7816783f490dec35594776f4d50
elijmcd <elijmcd@gmail.com>
2022-03-10 13:41:59 -0600

    Add project files.

---
+++ Adventure/Adventure.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using Engine;
+
+namespace Adventure
+{
+    public partial class Adventure : Form
+    {
+        private Player _player;
+
+        public Adventure()
+        {
+            InitializeComponent();
+
+            _player = new Player();
+
+            _player.CurrentHitpoints = 10;
+            _player.MaximumHitpoints = 10;
+            _player.Gold = 20;
+            _player.ExperiencePoints = 0;
+            _player.Level = 1;
+
+            lblHitpoints.Text = _player.CurrentHitpoints.ToString();
+            lblGold.Text = _player.Gold.ToString();
+            lblExperience.Text = _player.ExperiencePoints.ToString();
+            lblLevel.Text = _player.Level.ToString();
+        }
+    }
+}
